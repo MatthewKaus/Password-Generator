bd574ffa084c555a52c8657deb474b0f1a16032c
Matthew Kaus <matthewkausfox@gmail.com>
2022-01-14 03:42:19 -0700

    generator functions, needs refinement

--- Develop/script.js
+++ Develop/script.js
@@ -68,7 +68,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
